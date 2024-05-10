508a45a40e3e50265ad55f2064cdbb8864d45402
jaheda8256 <jahedasultana47@gmail.com>
2024-05-10 18:43:30 +0600

    added get and post

--- index.js
+++ index.js
@@ -2,21 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 //middleware
-app.use(cors(
-  { 
-     origin: ['http://localhost:5173'],
-    credentials: true
-    }
-));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wotzmkf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -26,39 +23,50 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    const foodCollection = client.db("foodsDB").collection("foods");
 
-  // auth related api
-  app.post("/jwt", async (req, res) => {
-    const user = req.body;
-    console.log('user for token',user);
-    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-      expiresIn: "1h",
+    // auth related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log("user for token", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+
+      res
+        //   .cookie("token", token, cookieOption)
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+          // sameSite: 'none'
+        })
+        .send({ success: true });
     });
 
-  
-    res
 
-    //   .cookie("token", token, cookieOption)
-     
-    .cookie('token', token,{
-        httpOnly: true,
-        secure: false,
-        // sameSite: 'none'
-    })
-    .send({ success: true });
-  });
-
-
+    app.get("/foods", async (req, res) => {
+        const cursor = foodCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      });
+      
+      
+      app.post("/foods", async (req, res) => {
+        const food = req.body;
+        console.log(food);
+        const result = await foodCollection.insertOne(food);
+        res.send(result);
+      });
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -66,13 +74,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 app.get("/", (req, res) => {
-    res.send("food server is running");
-  });
-  
-  app.listen(port, () => {
-    console.log(` food server is running on port: ${port}`);
-  });
-  
+  res.send("food server is running");
+});
+
+app.listen(port, () => {
+  console.log(` food server is running on port: ${port}`);
+});
